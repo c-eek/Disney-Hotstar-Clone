5f11e68ef528daa678b9cbaada09806c6f31421b
c-eek <153419213+c-eek@users.noreply.github.com>
2023-12-10 13:23:30 +0530

    Add files via upload

---
+++ slider-data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'WI  vs IND 317/8 (50)',
+        des: 'While Mandhana smashed her fifth ODI hundred, Kaur had 10 hits to the fence and two maximums in her 107-ball 109',
+        image: 'images/slider 6.PNG'
+    },
+    {
+        name: 'Falcon and The Winter Soldier',
+        des: 'Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'Loki',
+        des: 'After stealing the Tesseract, Loki comes into contact with a mysterious organization that gives him an ominous ultimatum, either fix the timeline or cease to exist completely.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'Wanda Vision',
+        des: 'Vision and Wanda live a normal life in Westview and conceal their superpowers. However, as decades pass by, they start doubting that everything is not what it seems.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'Raya and The Last Dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Luca',
+        des: 'Luca, a sea monster, befriends Alberto, another one of his kind who takes him on a land adventure. He experiences an exciting summer while keeping his parents in the dark.',
+        image: 'images/slider 5.PNG'
+    }
+]
